599f8d224dcada4e22a6a9d403bc2c15f2873aa1
onlychild12 <58424980+onlychild12@users.noreply.github.com>
2021-03-01 23:18:59 +0800

    Update UControl.h

--- Useinclude/UControl.h
+++ Useinclude/UControl.h
@@ -6,8 +6,8 @@ void Initialize();//初始化使用detch函数分配信号和函数
 void litcontrol(std::string,Storage);//未分配的信号皆在此地
 public:
 UControl(EPoll_op*,int);//初始化
-void testresponse(Storage);
-void toother(Storage);
+// void testresponse(Storage);
+// void toother(Storage);
 // void runfunction(void(UControl::*function)(Storage),Storage s);
 };
 // void UControl::runfunction(void(UControl::*function)(Storage),Storage s)//保留不可删除
