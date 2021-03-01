a636e55c700fa19ced49fb45e3e2ef5b7dfe3b26
onlychild12 <58424980+onlychild12@users.noreply.github.com>
2021-03-01 23:17:57 +0800

    Update UControl.cpp

--- Useinclude/UControl.cpp
+++ Useinclude/UControl.cpp
@@ -7,8 +7,10 @@ Initializeself(this);//不可删除！
 
 void UControl::Initialize()//使用detch函数分配信号和函数
 {
-deptach("test",&UControl::testresponse);
-deptach("file",&UControl::toother);
+    
+    
+// deptach("test",&UControl::testresponse);
+// deptach("file",&UControl::toother);
 
 
 }
@@ -16,17 +18,17 @@ void UControl::litcontrol(std::string,Storage)//未分配的信号皆在此地
 {
 
 }
-void UControl::testresponse(Storage s)
-{
-    LitSend("recv","love",4);
-  Settitle(s.content);
-}
+// void UControl::testresponse(Storage s)
+// {
+//     LitSend("recv","love",4);
+//   Settitle(s.content);
+// }
 
-void UControl::toother(Storage s)
-{
+// void UControl::toother(Storage s)
+// {
       
-      Broadcasttoperson_m("test1","file",s.content,s.length);
-    // //    LitSend("file",tmp,s.length);
+//       Broadcasttoperson_m("test1","file",s.content,s.length);
+//     // //    LitSend("file",tmp,s.length);
 
-    // Broadcast_m("file",tmp,s.length);
-}
+//     // Broadcast_m("file",tmp,s.length);
+// }
